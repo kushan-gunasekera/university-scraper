6b29c399ce628ed90a1840a5fe55b7b510d9cbf6
Kushan Gunasekera <kushan.harshana@ayra.com.au>
2024-06-15 10:50:09 +0530

    handle errors in Yale

--- 1052. Yale University/Yale University.py
+++ 1052. Yale University/Yale University.py
@@ -19,6 +19,15 @@ HEADERS = {
 }
 MAIN_DOMAIN = 'https://courses.yale.edu'
 UNIVERSITY = 'Yale University'
+COMPLETED_COURSES = []
+
+try:
+    with open('completed_courses.txt', 'r') as file:
+        content = file.read()
+        for i in content.splitlines():
+            COMPLETED_COURSES.append(i)
+except FileNotFoundError:
+    print("The file 'completed_courses.txt' does not exist. Skipping processing.")
 
 
 def get_courses():
@@ -29,57 +38,73 @@ def get_courses():
     courses = []
     for tag in course_tags:
         url = tag.get('value')
-        # if not (url and url.startswith('/courses-az/')):
-        #     continue
         courses.append(url)
 
     return list(set(courses))
 
 
-def get_description(result):
-    course = {
-        result['code']: {
-            'course_code': result['code'],
-            'course_name': result['title']
+def get_description(srcdb, result):
+    try:
+        course = {
+            result['code']: {
+                'course_code': result['code'],
+                'course_name': result['title']
+            }
         }
-    }
-    linked_crns = result.get('linked_crns')
-    srcdb = result.get('srcdb')
-    crn = result.get('crn')
-    code = result.get('code')
-    data = {"group": f"code:{code}", "key": f"crn:{crn}", "srcdb": srcdb, "matched": f"crn:{linked_crns}"}
-    r = requests.post(f'{MAIN_DOMAIN}/api/?page=fose&route=details', headers=HEADERS, data=urllib.parse.quote_plus(json.dumps(data, separators=(',', ':'))))
-    course[result['code']]['course_description'] = r.json().get('description')
-    course[result['code']]['course_professor'] = result.get('instr')
-    return course
+        linked_crns = result.get('linked_crns')
+        srcdb_ = result.get('srcdb')
+        crn = result.get('crn')
+        code = result.get('code')
+        data = {"group": f"code:{code}", "key": f"crn:{crn}", "srcdb": srcdb_, "matched": f"crn:{linked_crns}"}
+        r = requests.post(f'{MAIN_DOMAIN}/api/?page=fose&route=details', headers=HEADERS, data=urllib.parse.quote_plus(json.dumps(data, separators=(',', ':'))))
+        course[result['code']]['course_description'] = r.json().get('description')
+        course[result['code']]['course_professor'] = result.get('instr')
+        return f'{srcdb}|{result["code"]}', course
+    except Exception as e:
+        print(f'Error: {e}')
+        return None, None
 
 
-def get_course(url):
-    print(url)
-    data = {"other":{"srcdb":url},"criteria":[{"field":"stat","value":"A"}]}
-    HEADERS['Referer'] = f'{MAIN_DOMAIN}/?srcdb={url}&stat=A'
+def get_course(srcdb):
+    print(srcdb)
+    data = {"other":{"srcdb":srcdb},"criteria":[{"field":"stat","value":"A"}]}
+    HEADERS['Referer'] = f'{MAIN_DOMAIN}/?srcdb={srcdb}&stat=A'
     r = requests.post(f'{MAIN_DOMAIN}/api/?page=fose&route=search&stat=A', headers=HEADERS, data=urllib.parse.quote_plus(json.dumps(data, separators=(',', ':'))))
     courses = {}
     results = r.json().get('results', [])
     if not results:
-        print(f'No results | {url}')
+        print(f'No results | {srcdb}')
         return courses
 
-    print(f'results: {len(results)} | {url}')
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        for i in as_completed(executor.submit(get_description, result) for result in results):
-            courses = {**courses, **i.result()}
+    print(f'results: {len(results)} | {srcdb}')
+    final_results = {}
+    for result in results:
+        if f'{srcdb}|{result["code"]}' not in COMPLETED_COURSES:
+            final_results[result['code']] = result
 
-    print(f'{len(courses)} courses in {url}')
+    print(f'final results: {len(final_results)} | {srcdb}')
+    files_keys = []
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        for i in as_completed(executor.submit(get_description, srcdb, result) for result in final_results.values()):
+            file_key, desc_details = i.result()
+            if file_key and desc_details:
+                files_keys.append(file_key)
+                courses = {**courses, **desc_details}
+
+    with open('completed_courses.txt', 'a') as file:
+        for file_key in files_keys:
+            file.write(f'{file_key}\n')
+
+    print(f'{len(courses)} courses in {srcdb}\n')
     return courses
 
 
 def main():
     # get_course('202401')
-    course_urls = get_courses()
     full_courses = {}
+    course_srcdb = get_courses()
     with ThreadPoolExecutor(max_workers=1) as executor:
-        for i in as_completed(executor.submit(get_course, course_url) for course_url in course_urls):
+        for i in as_completed(executor.submit(get_course, srcdb) for srcdb in course_srcdb):
             full_courses = {**full_courses, **i.result()}
 
     with open(f'{UNIVERSITY}.json', 'w') as json_file:
