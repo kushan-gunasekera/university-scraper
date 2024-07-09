cad9c5d4dbbca48ad01cad6e758734dc4a58bfcb
Kushan Gunasekera <kushan.harshana@ayra.com.au>
2024-07-09 14:31:59 +0530

    new uni added

--- 120. CA State University Sacramento/CA State University Sacramento.py
+++ 120. CA State University Sacramento/CA State University Sacramento.py
@@ -16,67 +16,65 @@ from lxml import html
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
-MAIN_DOMAIN = 'https://catalog.colorado.edu'
-UNIVERSITY = 'University of Colorado Boulder'
+MAIN_DOMAIN = 'https://catalog.csus.edu'
+UNIVERSITY = 'CA State University Sacramento'
 
 
 def get_courses():
-    # print(f'page_number: {page_number}')
     r = requests.get(f'{MAIN_DOMAIN}/courses-a-z/', headers=HEADERS)
     soup = BeautifulSoup(r.content, 'html.parser')
     course_tags = soup.find('div', id='atozindex').find_all('a')
 
     courses = []
     for tag in course_tags:
-        href = tag.get('href')
-        if not href or '#' in href:
+        if not tag.get('href'):
             continue
-        # if '#' in href:
-        #     continue
-        courses.append(href)
-
+        courses.append(tag['href'])
     return courses
 
 
 def get_course(url):
-    print(url)
     r = requests.get(f'{MAIN_DOMAIN}{url}', headers=HEADERS)
     soup = BeautifulSoup(r.content, 'html.parser')
-    course_tags = soup.find_all('div', class_='courseblock')
+    course_tags = soup.find_all('strong')
     courses = {}
     if not course_tags:
+        print(f'No courses | {url}')
         return courses
 
-    for tag in course_tags:
-        title = tag.find('p', 'courseblocktitle noindent').find('strong').text.strip().replace('\xa0', ' ')
-        try:
-            course_code, course_name = re.split(r"\(\d+\)", title)
-        except Exception as error:
-            print(f'url: {url}')
-            print(f'title: {title}')
-            print(f'error: {error}')
-        desc = tag.find('p', 'courseblockdesc')
-        if desc:
-            desc = desc.text.strip().replace('\xa0', ' ').split('Requisites')[0]
-        courses[course_code.strip()] = {
-            'course_code': course_code.strip(),
-            'course_name': course_name.strip(),
-            'course_description': desc,
-        }
+    text = None
+    try:
+        for tag in course_tags:
+            text = tag.text
+            parts = text.split(".", 1)
+            if len(parts) != 2:
+                continue
+            course_code = parts[0].strip()
+            course_name = parts[1].strip()
+            courses[f'{course_code}|{url}'] = course_name
+    except Exception as error:
+        print(f'{text} | {url}')
+        raise Exception(error)
 
+    print(f'{len(courses)} | {url}')
     return courses
 
 
 def main():
     full_courses = {}
+    urls = get_courses()
+    print(len(urls))
+
     with ThreadPoolExecutor(max_workers=100) as executor:
-        for i in as_completed(executor.submit(get_course, url) for url in get_courses()):
+        for i in as_completed(executor.submit(get_course, url) for url in urls):
             full_courses = {**full_courses, **i.result()}
 
     with open(f'{UNIVERSITY}.json', 'w') as json_file:
         json.dump(full_courses, json_file, indent=4)
 
-    header = ['course_code', 'course_name', 'course_description']
+    header = [
+        'course_code', 'course_name', 'course_description', 'course_professor'
+    ]
     workbook = xlsxwriter.Workbook(f'{UNIVERSITY}.xlsx')
     worksheet = workbook.add_worksheet()
     for col, header_name in enumerate(header):
@@ -87,6 +85,7 @@ def main():
         worksheet.write(row, 0, value.get('course_code'))
         worksheet.write(row, 1, value.get('course_name'))
         worksheet.write(row, 2, value.get('course_description'))
+        worksheet.write(row, 3, value.get('course_professor'))
         row += 1
 
     workbook.close()
