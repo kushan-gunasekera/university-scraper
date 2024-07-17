b5397978e64fc86093e7db5df10f985190995a18
Kushan Gunasekera <kushan.harshana@ayra.com.au>
2024-07-17 12:30:09 +0530

    new uni added

---
+++ 1060. Pamona College/Pamona College.py
@@ -0,0 +1,102 @@
+# Adelphi University
+import itertools
+import re
+import json
+import math
+import random
+import time
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures._base import as_completed
+
+import requests
+import xlsxwriter
+from bs4 import BeautifulSoup
+from lxml import html
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+MAIN_DOMAIN_1 = 'https://catalog.pomona.edu/content.php?catoid=45&catoid=45&navoid=9003&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D={page_number}#acalog_template_course_filter'
+UNIVERSITY = 'Pamona College'
+
+
+def get_courses(domain, page_number):
+    print(f'page_number: {page_number}')
+    r = requests.get(domain.format(page_number=page_number), headers=HEADERS)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    course_tags = soup.find('table', class_='table_default').find_all('a')
+
+    courses = {}
+    for tag in course_tags:
+        if not str(tag.get('href')).startswith('preview_course_nopop.php?'):
+            continue
+        text = tag.text.strip().replace('\xa0', ' ')
+        tag_split = text.split(' - ', 1)
+        if len(tag_split) == 2:
+            code = tag_split[0]
+            url = f'https://catalog.pomona.edu/{tag.get("href")}'
+            print(f'{code} - {url}')
+            res = requests.get(url, headers=HEADERS)
+            soup = BeautifulSoup(res.content, 'html.parser')
+            content = soup.find_all(string=True)
+            start_extracting = False
+            extracted_text = ""
+            count = 0
+
+            for element in content:
+                if start_extracting:
+                    count += 1
+                    if count >= 3:
+                        break
+                    if count == 2:
+                        print(element)
+                        extracted_text += element.strip() + " "
+                if "Credit:" in element:
+                    start_extracting = True
+
+            prof = ''
+            instructor_tag = soup.find(string="Instructor(s):")
+            if instructor_tag:
+                prof = instructor_tag.next_element
+            courses[code] = {
+                'course_code': code,
+                'course_name': tag_split[1],
+                'course_description': extracted_text,
+                'course_professor': prof,
+            }
+    return courses
+
+
+def main():
+    # get_courses(5)
+    # for page_number in range(1, 52):
+    #     get_courses(page_number)
+    full_courses = {}
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for i in as_completed(executor.submit(get_courses, MAIN_DOMAIN_1, page_number) for page_number in range(1, 27)):
+            full_courses = {**full_courses, **i.result()}
+
+    with open(f'{UNIVERSITY}.json', 'w') as json_file:
+        json.dump(full_courses, json_file, indent=4)
+
+    header = [
+        'course_code', 'course_name', 'course_description', 'course_professor'
+    ]
+    workbook = xlsxwriter.Workbook(f'{UNIVERSITY}.xlsx')
+    worksheet = workbook.add_worksheet()
+    for col, header_name in enumerate(header):
+        worksheet.write(0, col, header_name)
+
+    row = 1
+    for value in full_courses.values():
+        worksheet.write(row, 0, value.get('course_code'))
+        worksheet.write(row, 1, value.get('course_name'))
+        worksheet.write(row, 2, value.get('course_description'))
+        worksheet.write(row, 3, value.get('course_professor'))
+        row += 1
+
+    workbook.close()
+
+
+if __name__ == '__main__':
+    main()
