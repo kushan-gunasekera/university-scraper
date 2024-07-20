2d639769951f221b3498f0e76e4d09f6fb120049
Kushan Gunasekera <kushan.harshana@ayra.com.au>
2024-07-20 18:52:56 +0530

    new uni added

--- 611. Ramapo College of New Jersey/Ramapo College of New Jersey.py
+++ 611. Ramapo College of New Jersey/Ramapo College of New Jersey.py
@@ -59,7 +59,7 @@ def get_courses(domain, page_number):
 
 def main():
     full_courses = {}
-    with ThreadPoolExecutor(max_workers=1) as executor:
+    with ThreadPoolExecutor(max_workers=100) as executor:
         for i in as_completed(executor.submit(get_courses, MAIN_DOMAIN_1, page_number) for page_number in range(1, 19)):
             full_courses = {**full_courses, **i.result()}
 
