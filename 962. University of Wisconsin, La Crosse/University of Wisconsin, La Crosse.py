09b518f3ad06c1c68706697a47ff9a778d1eca72
Kushan Gunasekera <kushan.harshana@ayra.com.au>
2024-07-25 14:32:00 +0530

    new uni added

---
+++ 962. University of Wisconsin, La Crosse/University of Wisconsin, La Crosse.py
@@ -0,0 +1,92 @@
+import json
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures._base import as_completed
+
+import requests
+import xlsxwriter
+from bs4 import BeautifulSoup
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s:%(message)s [%(filename)s/%(funcName)s:%(lineno)d:%(threadName)s]\n', level=logging.INFO)
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+MAIN_DOMAIN = 'https://catalog.uwlax.edu'
+UNIVERSITY = 'University of CA Irvine'
+
+
+def get_courses(path):
+    logging.info('getting urls')
+    r = requests.get(f'{MAIN_DOMAIN}{path}', headers=HEADERS)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    course_tags = soup.find('div', id='atozindex').find_all('a')
+
+    courses = []
+    for tag in course_tags:
+        if not tag.get('href'):
+            continue
+        courses.append(tag['href'])
+    logging.info(f'{len(courses)} urls found')
+    return courses
+
+
+def get_course(url):
+    logging.info(f'loading {url}')
+    r = requests.get(f'{MAIN_DOMAIN}{url}', headers=HEADERS)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    course_tags = soup.find_all('div', class_='courseblock')
+    courses = {}
+    if not course_tags:
+        return courses
+
+    for tag in course_tags:
+        p_tags = tag.find_all('p', class_='courseblocktitle')
+        course_code = p_tags[0].text
+        course_name = p_tags[1].text
+        courses[course_code] = {
+            'course_code': course_code,
+            'course_name': course_name,
+        }
+
+        desc_div_tag = tag.find('p', class_='courseblockdesc')
+        try:
+            course_description = desc_div_tag.text.strip().replace('\xa0', ' ')
+            courses[course_code]['course_description'] = course_description
+        except:
+            pass
+
+    logging.info(f'{len(courses)} courses found for {url}')
+    return courses
+
+
+def main():
+    full_courses = {}
+    urls = []
+    urls.extend(get_courses('/undergraduate/coursedescriptions/'))
+    urls.extend(get_courses('/graduate/coursedescriptions/'))
+
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        for i in as_completed(executor.submit(get_course, url) for url in urls):
+            full_courses = {**full_courses, **i.result()}
+
+    with open(f'{UNIVERSITY}.json', 'w') as json_file:
+        json.dump(full_courses, json_file, indent=4)
+
+    header = ['course_code', 'course_name', 'course_description']
+    workbook = xlsxwriter.Workbook(f'{UNIVERSITY}.xlsx')
+    worksheet = workbook.add_worksheet()
+    for col, header_name in enumerate(header):
+        worksheet.write(0, col, header_name)
+
+    row = 1
+    for value in full_courses.values():
+        worksheet.write(row, 0, value.get('course_code'))
+        worksheet.write(row, 1, value.get('course_name'))
+        worksheet.write(row, 2, value.get('course_description'))
+        row += 1
+
+    workbook.close()
+
+
+if __name__ == '__main__':
+    main()
